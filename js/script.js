1b79c254bcb66d59b0936773913f29af9905522e
sara <tahersara1001@gmail.com>
2023-12-07 05:51:36 +0200

    added scripts.js file

--- js/script.js
+++ js/script.js
@@ -1,16 +1,22 @@
-// script.js
 
-// Wait for the DOM to be fully loaded
-document.addEventListener('DOMContentLoaded', function() {
-    // Find the "Find a Property" button
-    var ctaButton = document.querySelector('.cta-button');
-  
-    // Add a click event listener to the button
-    ctaButton.addEventListener('click', function(event) {
-      // Prevent the default behavior of the button (in this case, following the link)
-      event.preventDefault();
-  
-      // Log a message to the console
-      console.log('Button clicked! You can add more functionality here.');
-    });
+  // script.js
+
+// Wait for the DOM to be ready
+document.addEventListener("DOMContentLoaded", function(event) {
+  // Get the search form element
+  const searchForm = document.getElementById('search-form');
+
+  // Add event listener for the form submission
+  searchForm.addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    // Get the search input value
+    const searchTerm = document.getElementById('search-input').value;
+
+    // Perform a search based on the input value (Example: log to console)
+    console.log('Searching for: ' + searchTerm);
+
+    // You can make an AJAX request here to fetch search results and update the UI accordingly
+    // For simplicity, this example just logs the search term to the console
   });
+});
